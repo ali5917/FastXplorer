5d4fc1bd78e388b20cde0a1abb8a80df927bcfdc
Ismail Silat <ismailsilat7@gmail.com>
2025-12-07 10:39:12 +0500

    Draw sprite & implement rotation logic

--- src/player.cpp
+++ src/player.cpp
@@ -10,6 +10,9 @@ Player::Player(Vector2 startGridPos, float s)
             color(BLUE),
             score(INITIAL_SCORE),
             size(PLAYER_SIZE) {
+                textureStill = LoadTexture("assets/person-still.png");
+                textureWalking = LoadTexture("assets/person-walking.png");
+                rotation = 0.0f;
 }
 
 void Player::input() {
@@ -22,21 +25,20 @@ void Player::input() {
 
 void Player::move(float dt, const class Campus* campus) {
     if (direction.x != 0 || direction.y != 0) {
-        // Calculate new position
         Vector2 newPos = pos;
         newPos.x += direction.x * speed * dt;
         newPos.y += direction.y * speed * dt;
         
-        // Clamp to screen bounds
+        // clamping if out of bounds
         if (newPos.x < 0) newPos.x = 0;
         if (newPos.y < 0) newPos.y = 0;
         if (newPos.x + size > WINDOW_WIDTH) newPos.x = WINDOW_WIDTH - size;
         if (newPos.y + size > WINDOW_HEIGHT) newPos.y = WINDOW_HEIGHT - size;
         
-        // Check collision with obstacles if campus provided
+        // collision detection
         bool canMove = true;
         if (campus != nullptr) {
-            // Check all corners of the player bounding box
+            // check for corners
             Vector2 topLeft = pixelToGrid(newPos);
             Vector2 topRight = pixelToGrid({newPos.x + size, newPos.y});
             Vector2 bottomLeft = pixelToGrid({newPos.x, newPos.y + size});
@@ -48,7 +50,7 @@ void Player::move(float dt, const class Campus* campus) {
             }
         }
         
-        // Only update position if movement is valid
+        // move if valid
         if (canMove) {
             pos = newPos;
             gridPos = pixelToGrid(pos);
@@ -61,16 +63,19 @@ void Player::update(float dt, const class Campus* campus) {
     move(dt, campus);
 }
 
-void Player::draw() const {
-    // Draw player as a circle
-    DrawCircleV(getCenter(), size/2, color);
+void Player::draw() {
     
-    // Draw direction indicator
+    // rotation based on movement direction
     if (direction.x != 0 || direction.y != 0) {
-        Vector2 center = getCenter();
-        Vector2 dirEnd = {center.x + direction.x * size/2, 
-                         center.y + direction.y * size/2};
-        DrawLineEx(center, dirEnd, 3, WHITE);
+        rotation = atan2(direction.y, direction.x) * RAD2DEG + 90.0f;
+    }
+
+    Vector2 origin = {size/2, size/2};
+    
+    if (direction.x != 0 || direction.y != 0) {
+        DrawTexturePro(textureWalking, Rectangle{0, 0, (float)textureWalking.width, (float)textureWalking.height}, Rectangle{pos.x + size/2, pos.y + size/2, size, size}, origin, rotation, WHITE);
+    } else {
+        DrawTexturePro(textureStill, Rectangle{0, 0, (float)textureStill.width, (float)textureStill.height}, Rectangle{pos.x + size/2, pos.y + size/2, size, size}, origin, rotation, WHITE);
     }
 }
 
@@ -84,5 +89,5 @@ Vector2 Player::gridToPixel(Vector2 gridPos) {
 }
 
 Vector2 Player::pixelToGrid(Vector2 pixelPos) {
-    return Vector2{std::floor(pixelPos.x / CELL_WIDTH), std::floor(pixelPos.y / CELL_HEIGHT)};
+    return Vector2{(float)floor(pixelPos.x / CELL_WIDTH), (float)floor(pixelPos.y / CELL_HEIGHT)};
 }
